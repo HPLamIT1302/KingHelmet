2fa43ac97d5c59b66792f36ba23520453cb47378
HPLamIT1302 <hophuclam1996@gmail.com>
2024-04-23 20:30:48 +0700

    update form btn

--- js/getdatenow.js
+++ js/getdatenow.js
@@ -1,19 +1,16 @@
+// Function to format the current date
+function getCurrentDate() {
+    var today = new Date();
+    var weekday = today.toLocaleDateString('vi-VN', { weekday: 'long' });
+    var day = today.getDate();
+    var month = today.toLocaleDateString('vi-VN', { month: 'long' });
+    var year = today.getFullYear();
 
-    function getCurrentDate() {
-        var today = new Date();
-        var weekdays = ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
-        var months = ["Tháng Một", "Tháng Hai", "Tháng Ba", "Tháng Tư", "Tháng Năm", "Tháng Sáu", "Tháng Bảy", "Tháng Tám", "Tháng Chín", "Tháng Mười", "Tháng Mười Một", "Tháng Mười Hai"];
-        
-        var weekday = weekdays[today.getDay()];
-        var day = today.getDate();
-        var month = months[today.getMonth()];
-        var year = today.getFullYear();
+    return weekday + ', ' + day + ' ' + month + ' ' + year;
+}
 
-        return weekday + ', ' + day + ' ' + month + ' ' + year;
-    }
-
-    // Update date on page load
-    window.onload = function() {
-        var currentDate = getCurrentDate();
-        document.getElementById("dateNow").innerHTML = currentDate;
-    };
+// Update date on page load
+window.onload = function () {
+    var currentDate = getCurrentDate();
+    document.getElementById("dateNow").textContent = currentDate;
+};
